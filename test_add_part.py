4d9ad1babf762e0cf30508f28c9d72a00875d15f
Graham Keeth <graham.keeth@gmail.com>
2023-10-02 12:30:06 -0700

    Add tests for csv and sql output from Component

--- test_add_part.py
+++ test_add_part.py
@@ -10,9 +10,66 @@ from add_part import Resistor
 #   - Component.from_digikey() for each component type
 #   - digikey API calls (create_component_from_digikey_pn)
 #   - Component.get_digikey_common_data probably doesn't need to be tested
-# - Component.to_csv()
-# - Component.to_sql() - check resulting database instead?
-# - Component.get_create_table_string() - check resulting table instead?
+
+
+class TestComponentOutputs(unittest.TestCase):
+    def setUp(self):
+        # we use Resistor here as the simplest (?) subclass of Component,
+        # but we're really testing the baseclass export methods
+        self.base_dict = {
+                "IPN": "R_test",
+                "datasheet": "ds",
+                "description": "desc",
+                "keywords": "kw",
+                "value": "val",
+                "exclude_from_bom": 0,
+                "exclude_from_board": 0,
+                "kicad_symbol": "sym",
+                "kicad_footprint": "fp",
+                "manufacturer": "mfg",
+                "MPN": "mpn",
+                "distributor1": "dist1",
+                "DPN1": "dpn1",
+                "distributor2": "dist2",
+                "DPN2": "dpn2",
+                "resistance": "10k",
+                "tolerance": "1%",
+                "power": "0.125W",
+                "composition": "ThinFilm",
+                "package": "0603",
+                }
+        self.resistor = add_part.create_component_from_dict(self.base_dict)
+
+    def test_to_csv(self):
+        header = ("IPN,datasheet,description,keywords,value,exclude_from_bom,"
+                  "exclude_from_board,kicad_symbol,kicad_footprint,"
+                  "manufacturer,MPN,distributor1,DPN1,distributor2,DPN2,"
+                  "resistance,tolerance,power,composition,package\r\n")
+        values = ("R_test,ds,desc,kw,val,0,0,sym,fp,mfg,mpn,dist1,dpn1,dist2,"
+                  "dpn2,10k,1%,0.125W,ThinFilm,0603\r\n")
+        self.assertEqual(header + values, self.resistor.to_csv())
+        self.assertEqual(values, self.resistor.to_csv(header=False))
+
+    def test_to_sql(self):
+        sql_expected = ("INSERT INTO resistor VALUES("
+                        ":IPN, :datasheet, :description, :keywords, :value, "
+                        ":exclude_from_bom, :exclude_from_board, "
+                        ":kicad_symbol, :kicad_footprint, :manufacturer, "
+                        ":MPN, :distributor1, :DPN1, :distributor2, :DPN2, "
+                        ":resistance, :tolerance, :power, :composition, "
+                        ":package)")
+        sql, vals = self.resistor.to_sql()
+        self.assertEqual(sql_expected, sql)
+        self.assertEqual(self.base_dict, vals)
+
+    def test_get_create_table_string(self):
+        sql_expected = ("CREATE TABLE IF NOT EXIST resistor("
+                        "IPN PRIMARY KEY, datasheet, description, keywords, "
+                        "value, exclude_from_bom, exclude_from_board, "
+                        "kicad_symbol, kicad_footprint, manufacturer, MPN, "
+                        "distributor1, DPN1, distributor2, DPN2, resistance, "
+                        "tolerance, power, composition, package)")
+        self.assertEqual(sql_expected, self.resistor.get_create_table_string())
 
 
 class TestResistorUtils(unittest.TestCase):
