e2e2e0cb4df418ce036d0bf94072755d1b98f179
Graham Keeth <graham.keeth@gmail.com>
2023-11-08 11:24:50 -0500

    Refactor db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -11,10 +11,10 @@ from partdb import db
 class TestDatabaseFunctions(unittest.TestCase):
     db_path = "unittests.db"
 
-    def setUp(self):
-        self.backup_IPN_DUPLICATE_LIMIT = db.IPN_DUPLICATE_LIMIT
-        self.base_dict = {
-            "IPN": "R_test",
+    @staticmethod
+    def create_dummy_component(IPN="R_test"):
+        base_dict = {
+            "IPN": IPN,
             "datasheet": "ds",
             "description": "desc",
             "keywords": "kw",
@@ -35,20 +35,24 @@ class TestDatabaseFunctions(unittest.TestCase):
             "composition": "ThinFilm",
             "package": "0603",
         }
-        self.resistor = component.create_component_from_dict(self.base_dict)
+        return component.create_component_from_dict(base_dict)
 
+    def setUp(self):
+        self.backup_IPN_DUPLICATE_LIMIT = db.IPN_DUPLICATE_LIMIT
         db.initialize_database(self.db_path)
+        self.con = sqlite3.connect(f"file:{self.db_path}?mode=rw", uri=True)
+        self.cur = self.con.cursor()
+        self.resistor = self.create_dummy_component()
 
     def tearDown(self):
+        self.con.close()
         db.IPN_DUPLICATE_LIMIT = self.backup_IPN_DUPLICATE_LIMIT
         os.remove(self.db_path)
 
     def test_table_automatically_created(self):
         db.open_connection_and_add_component_to_db(self.db_path, self.resistor)
 
-        con = sqlite3.connect(f"file:{self.db_path}?mode=ro", uri=True)
-        cur = con.cursor()
-        res = cur.execute("SELECT name from sqlite_master").fetchall()
+        res = self.cur.execute("SELECT name from sqlite_master").fetchall()
 
         self.assertIn(("resistor",), res)
 
@@ -57,9 +61,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.resistor.columns["IPN"] = "R_test2"
         db.open_connection_and_add_component_to_db(self.db_path, self.resistor)
 
-        con = sqlite3.connect(f"file:{self.db_path}?mode=ro", uri=True)
-        cur = con.cursor()
-        res = cur.execute("SELECT IPN from resistor").fetchall()
+        res = self.cur.execute("SELECT IPN from resistor").fetchall()
 
         self.assertIn(("R_test",), res)
         self.assertIn(("R_test2",), res)
@@ -71,23 +73,19 @@ class TestDatabaseFunctions(unittest.TestCase):
             self.db_path, self.resistor, update=True
         )
 
-        con = sqlite3.connect(f"file:{self.db_path}?mode=ro", uri=True)
-        cur = con.cursor()
-        res = cur.execute("SELECT value from resistor").fetchall()
+        res = self.cur.execute("SELECT value from resistor").fetchall()
 
         self.assertNotIn(("val",), res)
         self.assertIn(("val2",), res)
 
     def test_auto_increment_IPN(self):
         db.IPN_DUPLICATE_LIMIT = 3
-        con = sqlite3.connect(f"file:{self.db_path}?mode=rw", uri=True)
         for n in range(db.IPN_DUPLICATE_LIMIT):
-            self.base_dict["value"] = f"val{n}"
-            r = component.create_component_from_dict(self.base_dict)
-            db.add_component_to_db(con, r, increment=True)
+            r = self.create_dummy_component()
+            r.columns["value"] = f"val{n}"
+            db.add_component_to_db(self.con, r, increment=True)
 
-        cur = con.cursor()
-        res = cur.execute("SELECT IPN, value from resistor").fetchall()
+        res = self.cur.execute("SELECT IPN, value from resistor").fetchall()
 
         self.assertIn(("R_test", "val0"), res)
         self.assertIn(("R_test_1", "val1"), res)
@@ -95,33 +93,29 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def test_too_many_duplicate_IPNs_increment(self):
         db.IPN_DUPLICATE_LIMIT = 3
-        con = sqlite3.connect(f"file:{self.db_path}?mode=rw", uri=True)
         for n in range(db.IPN_DUPLICATE_LIMIT):
-            self.base_dict["value"] = f"val{n}"
-            r = component.create_component_from_dict(self.base_dict)
-            db.add_component_to_db(con, r, increment=True)
+            r = self.create_dummy_component()
+            r.columns["value"] = f"val{n}"
+            db.add_component_to_db(self.con, r, increment=True)
 
         with self.assertRaises(db.TooManyDuplicateIPNsInTableError) as cm:
-            r = component.create_component_from_dict(self.base_dict)
-            db.add_component_to_db(con, r)
+            r = self.create_dummy_component()
+            db.add_component_to_db(self.con, r)
         e = cm.exception
         self.assertEqual("R_test", e.IPN)
         self.assertEqual("resistor", e.table)
-        con.close()
 
     def test_too_many_duplicate_IPNs(self):
-        con = sqlite3.connect(f"file:{self.db_path}?mode=rw", uri=True)
-        self.base_dict["value"] = "val"
-        r = component.create_component_from_dict(self.base_dict)
-        db.add_component_to_db(con, r, increment=False)
+        r = self.create_dummy_component()
+        r.columns["value"] = "val"
+        db.add_component_to_db(self.con, r, increment=False)
 
         with self.assertRaises(db.TooManyDuplicateIPNsInTableError) as cm:
-            r = component.create_component_from_dict(self.base_dict)
-            db.add_component_to_db(con, r, increment=False)
+            r = self.create_dummy_component()
+            db.add_component_to_db(self.con, r, increment=False)
         e = cm.exception
         self.assertEqual("R_test", e.IPN)
         self.assertEqual("resistor", e.table)
-        con.close()
 
 
 if __name__ == "__main__":
